2eda668f589772420a655bd9b8b1424916750b7f
Riccardo Toni <riccardotoni@Riccardos-MacBook-Pro.local>
2019-12-27 10:06:29 +0000

    new spring initializr project with JPA and WEB with the right name

--- back-end/src/main/java/com/codurance/guru/GuruApplication.java
+++ back-end/src/main/java/com/codurance/guru/GuruApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.codurance.guru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GuruApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GuruApplication.class, args);
 	}
 
 }
--- back-end/src/test/java/com/codurance/guru/GuruApplicationTests.java
+++ back-end/src/test/java/com/codurance/guru/GuruApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.codurance.guru;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class GuruApplicationTests {
 
 	@Test
 	void contextLoads() {
