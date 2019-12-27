d854db24d81c31aaac8c897fc0cd0ce584b21a31
Riccardo Toni <riccardotoni@Riccardos-MacBook-Pro.local>
2019-12-27 09:54:51 +0000

    new spring initializr project with JPA and WEB

--- back-end/src/main/java/com/example/demo/DemoApplication.java
+++ back-end/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.guru.guruback;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GuruBackApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GuruBackApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- back-end/src/test/java/com/example/demo/DemoApplicationTests.java
+++ back-end/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.guru.guruback;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GuruBackApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
