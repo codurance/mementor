c5b58141145dc72ea706354c8732344d8c4aeb5f
aclaudel <arnaud.claudel@codurance.com>
2020-01-06 19:33:18 +0000

    SkeletonController api moved to CraftspeopleController

--- src/main/java/com/codurance/guru/craftspeople/CraftspeopleController.java
+++ src/main/java/com/codurance/guru/craftspeople/CraftspeopleController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class CraftspeopleController {
 
@@ -14,6 +17,14 @@ public class CraftspeopleController {
     @GetMapping("/craftspeople/{craftspersonId}")
     public Craftsperson retrieveCraftsperson(@PathVariable Integer craftspersonId) {
         return craftspeopleService.retrieveCraftsperson(craftspersonId);
+    }
+
+    @GetMapping("/craftspeople")
+    public List<Craftsperson> retrieveAll() {
+        List<Craftsperson> data = new ArrayList<>();
+        data.add(new Craftsperson("Riccardo","Toni"));
+        data.add(new Craftsperson("Jose", "Campos"));
+        return data;
     }
 
 }
