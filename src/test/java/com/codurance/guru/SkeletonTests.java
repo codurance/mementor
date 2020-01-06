e1f25d21ff3ac362bacf8913b37d78731a0be6c8
aclaudel <arnaud.claudel@codurance.com>
2020-01-06 15:44:21 +0000

    replace RequestMapping with PostMapping and change feign value

--- src/test/java/com/codurance/guru/SkeletonTests.java
+++ src/test/java/com/codurance/guru/SkeletonTests.java
@@ -1,9 +1,6 @@
 package com.codurance.guru;
 
 import com.codurance.guru.infrastructure.restclient.ClientTest;
-import com.jayway.restassured.RestAssured;
-import org.junit.Ignore;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +9,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static com.jayway.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -24,22 +19,7 @@ public class SkeletonTests {
 
     @Autowired
     private ClientTest clientTest;
-
-    @BeforeEach
-    public void setBaseUri () {
-        RestAssured.port = 8080;
-        RestAssured.baseURI = "http://localhost";
-    }
-
-    @Ignore
-    @Test
-    public void getDataTest() {
-        get("/api/tdd/responseData")
-                .then()
-                .assertThat()
-                .body("data", equalTo("responseData"));
-    }
-
+    
     @Test
     public void client_api_test_call() {
         String expectedJSON = "{\"ok\":true}";
