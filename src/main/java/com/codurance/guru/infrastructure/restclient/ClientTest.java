b05765157aadcd5ca2f55daf91a12b37cc27001f
aclaudel <arnaud.claudel@codurance.com>
2020-01-06 15:44:21 +0000

    replace RequestMapping with PostMapping and change feign value

--- src/main/java/com/codurance/guru/infrastructure/restclient/ClientTest.java
+++ src/main/java/com/codurance/guru/infrastructure/restclient/ClientTest.java
@@ -1,13 +1,12 @@
 package com.codurance.guru.infrastructure.restclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(value = "slackTestCall", url = "https://slack.com/api/")
+@FeignClient(value = "SlackApi", url = "https://slack.com/api/")
 public interface ClientTest {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/api.test")
+    @PostMapping("/api.test")
     String testSlackApi();
 
 }
