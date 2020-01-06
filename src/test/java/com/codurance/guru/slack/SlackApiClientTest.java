fa7a9507a604081f77d61a628a6b9ef6cdf5dfc0
aclaudel <arnaud.claudel@codurance.com>
2020-01-06 17:50:27 +0000

    optimize annotations and imports

--- src/test/java/com/codurance/guru/slack/SlackApiClientTest.java
+++ src/test/java/com/codurance/guru/slack/SlackApiClientTest.java
@@ -1,21 +1,18 @@
 package com.codurance.guru.slack;
 
 import com.codurance.guru.GuruApplication;
-import com.codurance.guru.slack.SlackApiClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = GuruApplication.class)
-@TestPropertySource(value={"classpath:application.properties"})
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        classes = GuruApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SlackApiClientTest {
 
     @Autowired
@@ -25,7 +22,6 @@ public class SlackApiClientTest {
     public void client_api_test_call() {
         String expectedJSON = "{\"ok\":true}";
         String response = slackApiClient.apiTest();
-
         assertEquals(expectedJSON, response);
     }
 
