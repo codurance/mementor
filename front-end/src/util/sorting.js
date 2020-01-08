218ebefeb5d7ca47cfe3759cc921eb661e8861a9
aclaudel <arnaud.claudel@codurance.com>
2020-01-08 15:09:33 +0000

    mentees displayed

--- front-end/src/util/sorting.js
+++ front-end/src/util/sorting.js
@@ -1,10 +1,20 @@
 export const default_sort = (data) => {
-    return data
-        .sort((a, b) => {
-            return b['mentees'].length - a['mentees'].length})
-        .map((craftsperson) => {
-            if(craftsperson['mentor'] != null)
-                return 'Craftsperson: ' + craftsperson['firstName'] + ' ' + craftsperson['lastName'] + ' - Mentor: ' + craftsperson['mentor'].split(' ')[0] + ' ' + craftsperson['mentor'].split(' ')[1];
-            return 'Craftsperson: ' + craftsperson['firstName'] + ' ' + craftsperson['lastName'];
-        });
+    return data.map((craftsperson) => {
+       const person = 'Craftsperson: ' + craftsperson['firstName'] + ' ' + craftsperson['lastName'];
+
+       let mentor = '-';
+       if(craftsperson['mentor'] != null) {
+               mentor = craftsperson.mentor.firstName + ' ' + craftsperson.mentor.lastName;
+        }
+
+       let mentees = '-';
+       if(craftsperson.mentees.length > 0) {
+           mentees = craftsperson.mentees
+               .map(m => m.firstName + ' ' + m.lastName)
+               .reduce((m1, m2) => m1 + ', ' + m2);
+       }
+
+       return [person, 'Mentor: ' + mentor, 'Mentees: ' + mentees].join(' | ');
+       });
 };
+
