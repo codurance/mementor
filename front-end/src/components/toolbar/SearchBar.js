4941fa7af4c5145f9f5deee4f7b915c60366ba12
jpwenzelc <jose.campos@codurance.com>
2020-01-09 18:31:12 +0000

    bootstrap searchbar

--- front-end/src/components/toolbar/SearchBar.js
+++ front-end/src/components/toolbar/SearchBar.js
@@ -1,6 +1,19 @@
 import React from "react";
-import './SearchBar.css'
+import './SearchBar.css';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 export default function SearchBar(props){
-    return <input type="text" className="search-bar" onChange={(e) => props.onEnter(e.target.value)} placeholder="Search for craftsperson"/>
+    return (
+        <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroup-sizing-sm">Craftsperson</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+                onChange={(e) => props.onEnter(e.target.value)}
+                aria-label="Craftsperson searchbar"
+                aria-describedby="basic-addon1"
+            />
+      </InputGroup>
+      )
 }
