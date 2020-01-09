1f30f51bb56d01ae98680488218b347e60e01d25
jpwenzelc <jose.campos@codurance.com>
2020-01-09 18:49:06 +0000

    collapsable rows

--- front-end/src/components/list/SortableRow.js
+++ front-end/src/components/list/SortableRow.js
@@ -1,6 +1,22 @@
 import React from "react";
 import Craftsperson from "./Craftsperson";
+import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
 
 export default function SortableRow(props){
-    return <li><Craftsperson craftsperson={props.craftsperson}/></li>;
+    return (
+        <Accordion>
+            <Card>
+                <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    <Craftsperson craftsperson={props.craftsperson}/>
+                </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                <Card.Body>{props.craftsperson.mentees.length}</Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        </Accordion>
+    ) 
 }
