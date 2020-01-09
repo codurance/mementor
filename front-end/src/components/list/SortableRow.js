e2c5c59d82cd4d3d27f1d9ec0b857221e479a780
jpwenzelc <jose.campos@codurance.com>
2020-01-09 19:55:56 +0000

    finishing touches

--- front-end/src/components/list/SortableRow.js
+++ front-end/src/components/list/SortableRow.js
@@ -1,30 +1,36 @@
 import React from "react";
 import Craftsperson from "./Craftsperson";
-import Card from 'react-bootstrap/Card';
-import Accordion from 'react-bootstrap/Accordion';
-import Button from 'react-bootstrap/Button';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import './Craftsperson.css'
 
-export default function SortableRow(props){
-    return (
-        <Accordion>
-            <Card>
-                <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                    <Craftsperson craftsperson={props.craftsperson}/>
-                </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                <ListGroup>
-                    {props.craftsperson.mentees.map(mentee => {
-                            return <ListGroup.Item>{mentee.firstName + ' ' + mentee.lastName}</ListGroup.Item>
-                    })}
-                </ListGroup>   
-                     
-                </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-        </Accordion>
-    ) 
+export default function SortableRow(props) {
+  return (
+    <Accordion>
+      <div class="container">
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle className="craftsperson-row" as={Button} variant="light" eventKey="0">
+              <Craftsperson craftsperson={props.craftsperson} />
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <ListGroup>
+                {props.craftsperson.mentees.map(mentee => {
+                  return (
+                    <ListGroup.Item>
+                      <h4>{mentee.firstName + " " + mentee.lastName}</h4>
+                    </ListGroup.Item>
+                  );
+                })}
+              </ListGroup>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </div>
+    </Accordion>
+  );
 }
