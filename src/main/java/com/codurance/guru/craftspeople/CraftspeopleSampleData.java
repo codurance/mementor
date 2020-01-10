ff01137a653a3d310e52b3d5c41385e50c344b75
jpwenzelc <jose.campos@codurance.com>
2020-01-10 12:06:30 +0000

    added real data to h2 db

--- src/main/java/com/codurance/guru/craftspeople/CraftspeopleSampleData.java
+++ src/main/java/com/codurance/guru/craftspeople/CraftspeopleSampleData.java
@@ -13,17 +13,34 @@ public class CraftspeopleSampleData {
 
     @EventListener
     public void appReady(ApplicationReadyEvent event) {
-        Craftsperson sandro = repository.save(new Craftsperson("Sandro", "Mancuso"));
-        Craftsperson steve = repository.save(new Craftsperson("Steve", "Lydford"));
-        Craftsperson stephane = repository.save(new Craftsperson("Stephane", "Meny"));
-        Craftsperson arnaud = repository.save(new Craftsperson("Arnaud", "CLAUDEL", sandro));
-        repository.save(new Craftsperson("Jose", "Wenzel", sandro));
-        repository.save(new Craftsperson("Etienne", "Mustow", sandro));
-        repository.save(new Craftsperson("Ed", "Rixon", steve));
-        repository.save(new Craftsperson("Michael", "Jackson",steve));
-        repository.save(new Craftsperson("Giulio", "Perrone", steve));
-        repository.save(new Craftsperson("Mahatma", "Gandhi", arnaud));
-        repository.save(new Craftsperson("Bob", "Martin", arnaud));
-        repository.save(new Craftsperson("Lev", "Boris", stephane));
+
+        Craftsperson mashB = repository.save(new Craftsperson("Mashooq", "Badar"));
+        Craftsperson sandroM = repository.save(new Craftsperson("Sandro", "Mancuso"));
+        Craftsperson steveL = repository.save(new Craftsperson("Steve", "Lydford"));
+        Craftsperson chrisE = repository.save(new Craftsperson("Christopher", "Eyre", steveL));
+        Craftsperson liamG = repository.save(new Craftsperson("Liam", "Griffin-Jowett", chrisE));
+        Craftsperson chrisB = repository.save(new Craftsperson("Chris", "Bimson", sandroM));
+        Craftsperson neilK = repository.save(new Craftsperson("Neil", "Kidd", chrisB));
+        Craftsperson richW = repository.save(new Craftsperson("Richard", "Wild", steveL));
+        Craftsperson jorgeG = repository.save(new Craftsperson("Jorge", "Gueorguiev", mashB));
+        Craftsperson alfredoF = repository.save(new Craftsperson("Alfredo", "Fernández", sandroM));
+        Craftsperson francescoG = repository.save(new Craftsperson("Francesco", "Gigli", chrisB));
+        Craftsperson danB = repository.save(new Craftsperson("Dan", "Bunea", mashB));
+        Craftsperson jordiC = repository.save(new Craftsperson("Jordi", "Creus", danB));
+        repository.save(new Craftsperson("Bartomeu", "Galmés", jordiC));
+        repository.save(new Craftsperson("Scott", "Edwards", steveL));
+        repository.save(new Craftsperson("Rory", "Collins", chrisE));
+        repository.save(new Craftsperson("Chris", "Reinmuller", alfredoF));
+        repository.save(new Craftsperson("Pierre-Jean", "Baraud", francescoG));
+        repository.save(new Craftsperson("Luciano", "Palma", sandroM));
+        repository.save(new Craftsperson("Mattsi", "Jansky", neilK));
+        repository.save(new Craftsperson("Alasdair", "Smith", liamG));
+        repository.save(new Craftsperson("Andrew", "Pattison", mashB));
+        repository.save(new Craftsperson("Sam", "Davies", richW));
+        repository.save(new Craftsperson("Tony", "Georgiadis", richW));
+        repository.save(new Craftsperson("Sandro", "Viljoen", liamG));
+        repository.save(new Craftsperson("Christina", "Ripley", jorgeG));
+        repository.save(new Craftsperson("Kryszotof", "Napierala", jorgeG));
+
     }
 }
