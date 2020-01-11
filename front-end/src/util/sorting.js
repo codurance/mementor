eb0faba70f93ca904eadac2aad8823d6c1d9e48d
pepperrone <giulio.perrone@codurance.com>
2020-01-11 00:15:24 +0000

    Fixed the property access notation to align it with the stablished convention

--- front-end/src/util/sorting.js
+++ front-end/src/util/sorting.js
@@ -1,11 +1,11 @@
-export const default_sort = (data) => {
+export const default_sort = function applyDefaultSortToCraftpeople(data){
     return data
         .sort((leftCraftsperson, rightCraftsperson) => {
-            if (leftCraftsperson['mentees'].length || rightCraftsperson['mentees'].length){
-                return rightCraftsperson['mentees'].length - leftCraftsperson['mentees'].length;
+            if (leftCraftsperson.mentees.length || rightCraftsperson.mentees.length){
+                return rightCraftsperson.mentees.length - leftCraftsperson.mentees.length;
             }
             else{
-                return (leftCraftsperson['firstName'] + leftCraftsperson['lastName']).localeCompare(rightCraftsperson['firstName'] + rightCraftsperson['lastName']);
+                return (leftCraftsperson.firstName + leftCraftsperson.lastName).localeCompare(rightCraftsperson.firstName + rightCraftsperson.lastName);
             }
          });
 };
