14d987ffa8ea1855a7d86ca20000421f6244102b
Etienne Mustow <etienne.o.mustow@gmail.com>
2020-01-13 15:16:07 +0000

    edge cases test added

--- front-end/src/components/list/Craftsperson.test.js
+++ front-end/src/components/list/Craftsperson.test.js
@@ -8,15 +8,39 @@ describe ('when rendering a craftsperson', () => {
         const {getByTestId} = render(<Craftsperson craftsperson={{firstName: "Arnaud", lastName: "Claudel"}} />)
         expect(getByTestId('craftspersonName')).toHaveTextContent('Arnaud Claudel')
     })
-    it('should show the mentor', () => {
-        const {getByTestId} = render(<Craftsperson craftsperson={{mentor: {firstName: "Etienne", lastName: "Mustow"}}} />)
-        expect(getByTestId('craftspersonMentorLabel')).toHaveTextContent('Mentored by:')
-        expect(getByTestId('craftspersonMentorValue')).toHaveTextContent('Etienne Mustow')
+
+    describe('with his mentor', () => {
+        it('should show the label', () => {
+            const {getByTestId} = render(<Craftsperson craftsperson={{mentor: null}} />)
+            expect(getByTestId('craftspersonMentorLabel')).toHaveTextContent('Mentored by:')
+        })
+        it('should show the mentor name', () => {
+            const {getByTestId} = render(<Craftsperson craftsperson={{mentor: {firstName: "Etienne", lastName: "Mustow"}}} />)
+            expect(getByTestId('craftspersonMentorLabel')).toHaveTextContent('Mentored by:')
+            expect(getByTestId('craftspersonMentorValue')).toHaveTextContent('Etienne Mustow')
+        })
+        it('should show a dash if there is no mentor', () => {
+            const {getByTestId} = render(<Craftsperson craftsperson={{mentor: null}}/>)
+            expect(getByTestId('craftspersonMentorValue')).toHaveTextContent('-')
+        })
     })
-    it('should show the number mentees', () => {
-        const listOfMentees = [{firstName: "Etienne", lastName: "Mustow"}, {firstName: "Arnaud", lastName: "Claudel"}, {firstName: "Naruto", lastName: "Uzumaki"}]
-        const {getByTestId} = render(<Craftsperson craftsperson={{mentees: listOfMentees}} />)
-        expect(getByTestId('craftspersonMenteeLabel')).toHaveTextContent('Mentees:')
-        expect(getByTestId('craftspersonMenteeValue')).toHaveTextContent('3')
-    })
+
+    describe('with his mentees', () => {
+        it('should show the label', () => {
+            const {getByTestId} = render(<Craftsperson craftsperson={{mentees: null}} />)
+            expect(getByTestId('craftspersonMenteeLabel')).toHaveTextContent('Mentees:')
+        })
+        it('should show the number mentees', () => {
+            const listOfMentees = [
+                    {firstName: "Etienne", lastName: "Mustow"}, 
+                    {firstName: "Arnaud", lastName: "Claudel"}, 
+                    {firstName: "Naruto", lastName: "Uzumaki"}]
+            const {getByTestId} = render(<Craftsperson craftsperson={{mentees: listOfMentees}} />)
+            expect(getByTestId('craftspersonMenteeValue')).toHaveTextContent('3')
+        })
+        it('should show 0 if the list is null', () => {
+            const {getByTestId} = render(<Craftsperson craftsperson={{mentees: null}} />)
+            expect(getByTestId('craftspersonMenteeValue')).toHaveTextContent('0')
+        })
+    })  
 })
