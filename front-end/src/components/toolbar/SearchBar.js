040013cfeca3bd9046f15cd2a6282e517305ca5f
jpwenzelc <jose.campos@codurance.com>
2020-01-14 10:51:13 +0000

    clear search

--- front-end/src/components/toolbar/SearchBar.js
+++ front-end/src/components/toolbar/SearchBar.js
@@ -1,38 +1,46 @@
 import React, { useState } from "react";
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
-import ReactDOM from 'react-dom'
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import ReactDOM from "react-dom";
 
-export default function SearchBar(props){
+export default function SearchBar(props) {
+  const [searchValue, setSearchValue] = useState(null);
+  var searchInput = null;
 
-    const [searchValue, setSearchValue] = useState(null);
-    var searchInput = null;
+  const clearSearch = () => {
+    setSearchValue("");
+    props.onEnter("");
+    ReactDOM.findDOMNode(searchInput).value = "";
+  };
 
-    const clearSearch = () => {
-        setSearchValue(""); 
-        props.onEnter("");
-        ReactDOM.findDOMNode(searchInput).value = "";
-    }
-
-    return (
-        <InputGroup className="search-bar mb-3">
-            <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroup-sizing-sm">Search</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl
-                ref={ searchInputElement => searchInput = searchInputElement}
-                className="searchBarInput"
-                data-testid="SearchBarInput"
-                onChange={(e) => {
-                    props.onEnter(e.target.value);
-                    setSearchValue(e.target.value);
-                }}
-                aria-label="Craftsperson searchbar"
-                aria-describedby="basic-addon1"
-                placeholder="Find Craftsperson..."
-            />
-            {searchValue && <Button className="clear-search" onClick={clearSearch} data-testid="clearSearchButton" variant="Light">Clear search</Button>}
-      </InputGroup>
-      )
+  return (
+    <InputGroup className="search-bar mb-3">
+      <InputGroup.Prepend>
+        <InputGroup.Text id="inputGroup-sizing-sm">Search</InputGroup.Text>
+      </InputGroup.Prepend>
+      <FormControl
+        ref={searchInputElement => (searchInput = searchInputElement)}
+        className="searchBarInput"
+        data-testid="SearchBarInput"
+        aria-label="Craftsperson searchbar"
+        aria-describedby="basic-addon1"
+        placeholder="Find Craftsperson..."
+        onChange={e => {
+          props.onEnter(e.target.value);
+          setSearchValue(e.target.value);
+        }}
+      />
+      {searchValue && (
+        <Button
+          className="clear-search"
+          onClick={clearSearch}
+          data-testid="clearSearchButton"
+          variant="Light"
+        >
+          Clear search
+        </Button>
+      )}
+    </InputGroup>
+  );
 }
--- front-end/src/components/toolbar/Searchbar.test.js
+++ front-end/src/components/toolbar/Searchbar.test.js
@@ -1,19 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitForElementToBeRemoved } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
 
 import SearchBar from "./SearchBar";
 
 describe("Search Component", () => {
-  it("should have a Button with the 'Clear Search' label", () => {
-    const { getByTestId } = render(<SearchBar />);
+  describe("With text", () => {
+    it("should have a Button with the 'Clear Search' label", () => {
+      const { getByTestId } = render(<SearchBar onEnter={() => 1} />);
 
-    expect(getByTestId("clearSearchButton")).toHaveTextContent("Clear search");
+      userEvent.type(getByTestId("SearchBarInput"), "Hello");
+
+      expect(getByTestId("clearSearchButton")).toBeInTheDocument();
+      expect(getByTestId("clearSearchButton")).toHaveTextContent(
+        "Clear search"
+      );
+    });
   });
+  describe("Without text", () => {
+    it("should not display a <Button />", () => {
+      const { queryByTestId } = render(<SearchBar onEnter={() => 1} />);
 
-  it("should display a <Button /> when is not empty", () => {
-    const { getByTestId } = render(<SearchBar />);
+      expect(queryByTestId("clearSearchButton")).toBeNull();
+    });
+    it("should not display when the input was cleared", async () => {
+      const { getByTestId, queryByTestId } = render(
+        <SearchBar onEnter={() => 1} />
+      );
 
-    expect(getByTestId("clearSearchButton")).not.toHaveClass("is-hidden");
+      userEvent.type(getByTestId("SearchBarInput"), "Hello");
+      expect(getByTestId("clearSearchButton")).toBeInTheDocument();
+
+      userEvent.click(getByTestId("clearSearchButton"));
+      await 400;
+      expect(queryByTestId("clearSearchButton")).not.toBeInTheDocument();
+    });
   });
 });
