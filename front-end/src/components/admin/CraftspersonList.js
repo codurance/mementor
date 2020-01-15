b1cc598c113295a51907c7484a10762efb66155e
jpwenzelc <jose.campos@codurance.com>
2020-01-15 18:18:56 +0000

    fix rerendering of the main list

--- front-end/src/components/admin/CraftspersonList.js
+++ front-end/src/components/admin/CraftspersonList.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import "./CraftspersonList.css";
-import { sortAlphabetically } from "../../util/sorting"
+import { sortAlphabetically } from "../../util/sorting";
 
 export default function CraftspersonList(props) {
   return (
     <ListGroup variant="flush" data-testid="craftspersonList">
-      {props.craftspeople.sort(sortAlphabetically).map(craftsperson => (
-        <ListGroup.Item data-testid="craftspersonRowItem" action onClick={() => props.click(craftsperson.id)} eventKey={craftsperson.id}>
+      {props.craftspeople.slice().sort(sortAlphabetically).map(craftsperson => (
+        <ListGroup.Item
+          data-testid="craftspersonRowItem"
+          action
+          onClick={() => props.click(craftsperson.id)}
+          eventKey={craftsperson.id}
+        >
           {craftsperson.firstName} {craftsperson.lastName}
         </ListGroup.Item>
       ))}
