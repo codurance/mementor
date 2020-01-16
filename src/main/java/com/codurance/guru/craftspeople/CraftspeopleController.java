0a07256dde5291018551eee6b455b525ece537ed
aclaudel <arnaud.claudel@codurance.com>
2020-01-16 16:28:39 +0000

    imports fixed

--- src/main/java/com/codurance/guru/craftspeople/CraftspeopleController.java
+++ src/main/java/com/codurance/guru/craftspeople/CraftspeopleController.java
@@ -1,6 +1,8 @@
 package com.codurance.guru.craftspeople;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
