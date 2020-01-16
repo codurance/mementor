e370600b410bea5e969e6c3329c724d76c127e1c
Riccardo Toni <riccardotoni@Riccardos-MacBook-Pro.local>
2020-01-16 15:20:36 +0000

    fixed missing added files

---
+++ front-end/src/components/list/Mentees.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import ListGroup from "react-bootstrap/ListGroup";
+import Mentee from './Mentee';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import {sortAlphabetically} from '../../util/sorting';
+import { api } from '../../util/api';
+
+
+export default function Mentees(props) {
+    
+    function addMentee(mentee) {
+        if(mentee != null){
+            api('craftspeople/addmentee', {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    'mentorId': props.craftsperson.id,
+                    'menteeId': mentee.id
+                })
+            });
+        }
+        props.rerender();
+    }
+
+    return (
+        <ListGroup data-testid="list">
+            {props.mentees.sort(sortAlphabetically).map(mentee => <Mentee key={mentee.id} rerender={props.rerender} mentee={mentee} />)}
+            <ListGroupItem data-testid='add-mentee-row'>
+                <h4>Add mentee</h4>
+                <Typeahead
+                    labelKey={(option) => `${option.firstName} ${option.lastName}`}
+                    options={props.craftspeople}
+                    placeholder="Select a mentor"
+                    onChange={(selected) => addMentee(selected[0])}
+                />
+            </ListGroupItem>
+        </ListGroup>
+    );
+}
