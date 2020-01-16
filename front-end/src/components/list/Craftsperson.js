8623813f437084db3af01c7c6679174ba9015ebb
aclaudel <arnaud.claudel@codurance.com>
2020-01-16 20:16:01 +0000

    number of mentees refactored

--- front-end/src/components/list/Craftsperson.js
+++ front-end/src/components/list/Craftsperson.js
@@ -12,10 +12,10 @@ export default function Craftsperson({ craftsperson }) {
         <h5><span className='mentorLabel' data-testid='craftspersonMentorLabel'>Mentored by:</span></h5>
       </div>
       <div className='col-lg-4'>
-        <span className='menteeLabel' data-testid='craftspersonMenteeLabel'>Mentees: </span>
         <span className='mentee-count'>
           <h2 data-testid='craftspersonMenteeValue'>{craftsperson.mentees ? craftsperson.mentees.length : '0'}</h2>
         </span>
+        <span className='menteeLabel' data-testid='craftspersonMenteeLabel'><i>Mentees</i></span>
       </div>
     </div>
   );
