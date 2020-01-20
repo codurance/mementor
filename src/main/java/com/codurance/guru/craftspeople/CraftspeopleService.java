b4d8ce50f378373e2dc2a771cac7e61b80ab058e
edtheshed <edward.rixon@codurance.com>
2020-01-20 18:17:06 +0000

    added last meeting tests

--- src/main/java/com/codurance/guru/craftspeople/CraftspeopleService.java
+++ src/main/java/com/codurance/guru/craftspeople/CraftspeopleService.java
@@ -1,9 +1,11 @@
 package com.codurance.guru.craftspeople;
 
+import com.codurance.guru.craftspeople.exceptions.InvalidLastMeetingDateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +14,10 @@ public class CraftspeopleService {
 
     @Autowired
     private CraftspeopleRepository repository;
+
+    public CraftspeopleService(CraftspeopleRepository repository) {
+        this.repository = repository;
+    }
 
     public Optional<Craftsperson> retrieveCraftsperson(Integer craftspersonId) {
         return repository.findById(craftspersonId);
@@ -69,8 +75,14 @@ public class CraftspeopleService {
     }
 
     public void setLastMeeting(int craftspersonId, int lastMeeting) {
+        Instant lastMeetingInstant = Instant.ofEpochSecond(lastMeeting);
+
+        if (lastMeetingInstant.isAfter(Instant.now().plus(1, ChronoUnit.DAYS))){
+            throw new InvalidLastMeetingDateException();
+        }
+
         Craftsperson craftsperson = repository.findById(craftspersonId).get();
-        craftsperson.setLastMeeting(Instant.ofEpochSecond(lastMeeting));
+        craftsperson.setLastMeeting(lastMeetingInstant);
         repository.save(craftsperson);
     }
 }
