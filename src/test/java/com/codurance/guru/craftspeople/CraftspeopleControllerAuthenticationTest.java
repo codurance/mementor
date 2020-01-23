1c55069e85155086ae3b7ce9166a43393ebe54d2
edtheshed <edward.rixon@codurance.com>
2020-01-23 12:20:39 +0000

    fixed tests

--- src/test/java/com/codurance/guru/craftspeople/CraftspeopleControllerAuthenticationTest.java
+++ src/test/java/com/codurance/guru/craftspeople/CraftspeopleControllerAuthenticationTest.java
@@ -6,15 +6,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 
 @RunWith(SpringRunner.class)
+@DirtiesContext
 @SpringBootTest(
         classes = GuruApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-        properties = { "should.filter.requests=true" })
+        properties = {"should.filter.requests=true"})
 public class CraftspeopleControllerAuthenticationTest {
 
     private JSONObject requestBody;
