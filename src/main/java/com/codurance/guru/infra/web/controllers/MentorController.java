4410181d0cfcf03456a8adcd3029adadbc9df290
aclaudel <arnaud.claudel@codurance.com>
2020-01-26 22:41:02 +0000

    remove duplication of /craftspeople/mentor

--- src/main/java/com/codurance/guru/infra/web/controllers/MentorController.java
+++ src/main/java/com/codurance/guru/infra/web/controllers/MentorController.java
@@ -1,38 +1,34 @@
 package com.codurance.guru.infra.web.controllers;
 
 import com.codurance.guru.core.craftspeople.CraftspeopleService;
-import com.codurance.guru.core.craftspeople.Craftsperson;
 import com.codurance.guru.core.craftspeople.exceptions.DuplicateMenteeException;
-import com.codurance.guru.core.craftspeople.exceptions.ExistingCraftspersonException;
-import com.codurance.guru.core.craftspeople.exceptions.InvalidLastMeetingDateException;
 import com.codurance.guru.core.craftspeople.exceptions.InvalidMentorRelationshipException;
-import com.codurance.guru.infra.web.requests.AddCraftspersonRequest;
 import com.codurance.guru.infra.web.requests.AddMentorRequest;
 import com.codurance.guru.infra.web.requests.RemoveMentorRequest;
-import com.codurance.guru.infra.web.requests.UpdateLastMeetingRequest;
 import com.codurance.guru.infra.web.responses.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
 
-import static org.springframework.http.ResponseEntity.*;
+import static org.springframework.http.ResponseEntity.badRequest;
 
-@RestController
+@Controller
+@RequestMapping("craftspeople/mentor")
 public class MentorController {
 
     @Autowired
     private CraftspeopleService craftspeopleService;
 
-    @PostMapping("/craftspeople/mentor/add")
-    public ResponseEntity addMentor(@Valid @RequestBody AddMentorRequest request) {
+    @PostMapping("add")
+    public ResponseEntity<ErrorResponse> addMentor(@Valid @RequestBody AddMentorRequest request) {
         try {
             craftspeopleService.addMentor(request.getMentorId(), request.getMenteeId());
             return ResponseEntity.noContent().build();
-
         } catch (DuplicateMenteeException ex) {
             return badRequest().body(new ErrorResponse("The craftsperson is already mentoring that mentee."));
         } catch (InvalidMentorRelationshipException ex) {
@@ -40,7 +36,7 @@ public class MentorController {
         }
     }
 
-    @PostMapping("/craftspeople/mentor/remove")
+    @PostMapping("remove")
     public ResponseEntity<Void> removeMentor(@Valid @RequestBody RemoveMentorRequest request) {
         craftspeopleService.removeMentor(request.getMenteeId());
         return ResponseEntity.noContent().build();
