e4836d7ab5978e221a7c9bdebd885309ab281835
aclaudel <arnaud.claudel@codurance.com>
2020-01-26 18:51:36 +0000

    remove entity annotation from LastMeetingThreshold

--- src/main/java/com/codurance/guru/core/configuration/lastmeeting/threshold/LastMeetingThreshold.java
+++ src/main/java/com/codurance/guru/core/configuration/lastmeeting/threshold/LastMeetingThreshold.java
@@ -1,18 +1,7 @@
 package com.codurance.guru.core.configuration.lastmeeting.threshold;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Entity
 public class LastMeetingThreshold {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @Column
     private Integer lastMeetingThresholdsInWeeks;
 
     public LastMeetingThreshold() {}
