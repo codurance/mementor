305964779fc11e256c0e0330ba9dd8d84f3bae32
aclaudel <arnaud.claudel@codurance.com>
2020-01-27 11:21:25 +0000

    extract lastMeeting tests to a existing LastMeetingThresholdIT

--- src/test/java/com/codurance/guru/infra/CraftspeopleIT.java
+++ src/test/java/com/codurance/guru/infra/CraftspeopleIT.java
@@ -4,10 +4,7 @@ import com.codurance.guru.GuruApplication;
 import com.codurance.guru.core.craftspeople.Craftsperson;
 import com.codurance.guru.infra.persistence.repository.CraftspeopleRepositoryImpl;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,15 +14,9 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
@@ -41,39 +32,11 @@ public class CraftspeopleIT {
 
     private Craftsperson savedCraftsperson;
     private Craftsperson mentor;
-    private List<Craftsperson> craftspeople;
     private Response response;
-    private JSONObject requestBody;
-    private int lastMeetingEpoch;
 
     @Before
     public void setUp() {
-        requestBody = new JSONObject();
-        craftspeople = new ArrayList<>();
-        lastMeetingEpoch = 1500000000;
         RestAssured.port = serverPort;
-    }
-
-    @Test
-    public void can_update_a_craftsperson_last_meeting() throws JSONException {
-        given_a_craftsperson_with_a_mentor();
-        given_the_request_body_has_a_last_meeting_set();
-
-        when_the_last_meeting_is_set();
-
-        then_the_response_should_be_no_content();
-        then_the_last_meeting_is_updated();
-    }
-
-    @Test
-    public void cant_update_a_craftsperson_last_meeting_with_a_date_in_future() throws JSONException {
-        given_a_craftsperson_with_a_mentor();
-        given_the_request_body_has_a_last_meeting_set_in_the_future();
-
-        when_the_last_meeting_is_set();
-
-        then_the_response_should_be_bad_request();
-        then_the_response_should_contain_the_last_meeting_error();
     }
 
     @Test
@@ -110,16 +73,6 @@ public class CraftspeopleIT {
         savedCraftsperson = craftspeopleRepository.save(new Craftsperson("Arnaud", "CLAUDEL", mentor.getId(), Instant.now()));
     }
 
-    private void given_the_request_body_has_a_last_meeting_set() throws JSONException {
-        requestBody.put("craftspersonId", savedCraftsperson.getId());
-        requestBody.put("lastMeeting", lastMeetingEpoch);
-    }
-
-    private void given_the_request_body_has_a_last_meeting_set_in_the_future() throws JSONException {
-        requestBody.put("craftspersonId", savedCraftsperson.getId());
-        requestBody.put("lastMeeting", Instant.now().plus(2, ChronoUnit.DAYS).getEpochSecond());
-    }
-
     private void then_the_craftsperson_retrieved_has_a_mentor() {
         response.then().assertThat()
                 .body("id", equalTo(savedCraftsperson.getId()))
@@ -127,27 +80,6 @@ public class CraftspeopleIT {
                 .body("mentor.lastName", equalTo(mentor.getLastName()))
                 .body("mentor.id", equalTo(mentor.getId()))
                 .body("lastMeeting", equalTo((int)savedCraftsperson.getLastMeeting().get().getEpochSecond()));
-    }
-
-    private void then_the_last_meeting_is_updated() {
-        Optional<Instant> lastMeeting = craftspeopleRepository.findById(savedCraftsperson.getId()).get().getLastMeeting();
-        assertTrue(lastMeeting.isPresent());
-        assertEquals(Instant.ofEpochSecond(lastMeetingEpoch), lastMeeting.get());
-    }
-
-    private void then_the_response_should_be_bad_request() {
-        response.then().assertThat()
-                .statusCode(400);
-    }
-
-    private void then_the_response_should_be_no_content() {
-        response.then().assertThat()
-                .statusCode(204);
-    }
-
-    private void then_the_response_should_contain_the_last_meeting_error() {
-        response.then().assertThat()
-                .body("message", equalTo("The last meeting date is too far in the future"));
     }
 
     private void then_the_returned_craftsperson_is_the_correct_one() {
@@ -169,13 +101,6 @@ public class CraftspeopleIT {
 
     private void when_the_get_method_on_the_api_is_called_with_the_craftsperson_id() {
         response = RestAssured.get("craftspeople/{craftspersonId}", savedCraftsperson.getId());
-    }
-
-    private void when_the_last_meeting_is_set() {
-        response = given()
-                .contentType("application/json")
-                .body(requestBody.toString())
-                .put("craftspeople/lastmeeting");
     }
 
 }
--- src/test/java/com/codurance/guru/infra/LastMeetingIT.java
+++ src/test/java/com/codurance/guru/infra/LastMeetingIT.java
@@ -2,6 +2,8 @@ package com.codurance.guru.infra;
 
 import com.codurance.guru.GuruApplication;
 import com.codurance.guru.core.configuration.lastmeeting.threshold.LastMeetingThreshold;
+import com.codurance.guru.core.craftspeople.Craftsperson;
+import com.codurance.guru.infra.persistence.repository.CraftspeopleRepositoryImpl;
 import com.codurance.guru.infra.persistence.repository.LastMeetingThresholdRepositoryImpl;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
@@ -17,16 +19,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(
         classes = GuruApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class LastMeetingThresholdIT {
+public class LastMeetingIT {
 
     @Autowired
     private LastMeetingThresholdRepositoryImpl repository;
+
+    @Autowired
+    private CraftspeopleRepositoryImpl craftspeopleRepository;
+
+
     private ValidatableResponse response;
     private JSONObject body;
+    private Craftsperson savedCraftsperson;
+    private Craftsperson mentor;
+    private JSONObject requestBody;
+    private int lastMeetingEpoch;
 
     @LocalServerPort
     int serverPort;
@@ -34,6 +54,8 @@ public class LastMeetingThresholdIT {
     @Before
     public void setUp() {
         RestAssured.port = serverPort;
+        lastMeetingEpoch = 1500000000;
+        requestBody = new JSONObject();
     }
 
     @Test
@@ -94,6 +116,28 @@ public class LastMeetingThresholdIT {
         then_there_is_an_error_with("Last meeting threshold must be greater than 0");
     }
 
+    @Test
+    public void can_update_a_craftsperson_last_meeting() throws JSONException {
+        given_a_craftsperson_with_a_mentor();
+        given_the_request_body_has_a_last_meeting_set();
+
+        when_the_last_meeting_is_set();
+
+        then_the_response_should_be_no_content();
+        then_the_last_meeting_is_updated();
+    }
+
+    @Test
+    public void cant_update_a_craftsperson_last_meeting_with_a_date_in_future() throws JSONException {
+        given_a_craftsperson_with_a_mentor();
+        given_the_request_body_has_a_last_meeting_set_in_the_future();
+
+        when_the_last_meeting_is_set();
+
+        then_the_response_should_be_bad_request();
+        then_the_response_should_contain_the_last_meeting_error();
+    }
+
     private void given_a_configuration_with(int threshold) {
         repository.save(new LastMeetingThreshold(threshold));
     }
@@ -128,6 +172,50 @@ public class LastMeetingThresholdIT {
 
     private void then_there_is_an_error_with(String message) {
         response.statusCode(400)
-                .body("message", Matchers.equalTo(   message));
+                .body("message", Matchers.equalTo(message));
+    }
+
+    private void given_a_craftsperson_with_a_mentor() {
+        mentor = craftspeopleRepository.save(new Craftsperson("Jose", "Wenzel"));
+        savedCraftsperson = craftspeopleRepository.save(new Craftsperson("Arnaud", "CLAUDEL", mentor.getId(), Instant.now()));
+    }
+
+    private void given_the_request_body_has_a_last_meeting_set() throws JSONException {
+        requestBody.put("craftspersonId", savedCraftsperson.getId());
+        requestBody.put("lastMeeting", lastMeetingEpoch);
+    }
+
+    private void given_the_request_body_has_a_last_meeting_set_in_the_future() throws JSONException {
+        requestBody.put("craftspersonId", savedCraftsperson.getId());
+        requestBody.put("lastMeeting", Instant.now().plus(2, ChronoUnit.DAYS).getEpochSecond());
+    }
+
+    private void then_the_last_meeting_is_updated() {
+        Optional<Instant> lastMeeting = craftspeopleRepository.findById(savedCraftsperson.getId()).get().getLastMeeting();
+        assertTrue(lastMeeting.isPresent());
+        assertEquals(Instant.ofEpochSecond(lastMeetingEpoch), lastMeeting.get());
+    }
+
+    private void then_the_response_should_be_bad_request() {
+        response.assertThat()
+                .statusCode(400);
+    }
+
+    private void then_the_response_should_be_no_content() {
+        response.assertThat()
+                .statusCode(204);
+    }
+
+    private void then_the_response_should_contain_the_last_meeting_error() {
+        response.assertThat()
+                .body("message", equalTo("The last meeting date is too far in the future"));
+    }
+
+    private void when_the_last_meeting_is_set() {
+        response = given()
+                .contentType("application/json")
+                .body(requestBody.toString())
+                .put("craftspeople/lastmeeting")
+                .then();
     }
 }
