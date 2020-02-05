72edd46e437b78427733810bf56b9478c700b32f
aclaudel <arnaud.claudel@codurance.com>
2020-02-05 11:44:58 +0000

    display the entire CraftspersonRow component

--- front-end/src/stories/1-Craftsperson.stories.js
+++ front-end/src/stories/1-Craftsperson.stories.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import Craftsperson from "../components/list/Craftsperson";
+import CraftspersonRow from "../components/list/CraftspersonRow";
 import { LinkTo, linkTo } from '@storybook/addon-links'
 
 
 export default {
   title: 'Craftsperson',
-  component: Craftsperson,
+  component: CraftspersonRow,
 };
 
 export const Default = () =>
-  <Craftsperson
-    craftsperson={{id: 1, firstName: "Arnaud", lastName: "CLAUDEEL"}}
+  <CraftspersonRow
+    craftsperson={{id: 1, firstName: "Arnaud", lastName: "CLAUDEEL", mentees: []}}
     craftspeople={[]}
   />;
 
 export const WithAMentor = () =>
-  <Craftsperson
+  <CraftspersonRow
     craftsperson={
       {id: 1, firstName: "Arnaud", lastName: "CLAUDEEL",
-      mentor: {id: 3, firstName:"Jose", lastName: "Rixon"}
-      }}
+      mentor: {id: 3, firstName:"Jose", lastName: "Rixon"}, mentees: []}
+      }
     craftspeople={[]}
   />;
 
@@ -28,11 +28,11 @@ export const WithAnRecentLastMeeting = () =>
     <div>
       <LinkTo story="SortingBar">Go back</LinkTo>
       <button onClick={linkTo('Sorting bar')}>Go to "Default"</button>
-      <Craftsperson
+      <CraftspersonRow
         craftsperson={
           {id: 1, firstName: "Arnaud", lastName: "CLAUDEEL",
             mentor: {id: 3, firstName:"Jose", lastName: "Rixon"},
-            lastMeeting: new Date().getTime() / 1000
+            lastMeeting: new Date().getTime() / 1000, mentees: []
           }}
         lastMeetingThresholdsInWeeks={2}
         craftspeople= {[]}
@@ -40,17 +40,17 @@ export const WithAnRecentLastMeeting = () =>
     </div>);
 
 export const WithAnOldLastMeeting = () =>
-      <Craftsperson
+      <CraftspersonRow
         craftsperson={
           {id: 1, firstName: "Arnaud", lastName: "CLAUDEEL",
             mentor: {id: 3, firstName:"Jose", lastName: "Rixon"},
-            lastMeeting: 15_000_0000
+            lastMeeting: 15_000_0000, mentees: []
           }}
         craftspeople= {[]}
       />;
 
 export const With2Mentees = () =>
-  <Craftsperson
+  <CraftspersonRow
     craftsperson={
       {id: 1, firstName: "Arnaud", lastName: "CLAUDEEL",
         mentees: [
