a37b67b838af393673ee5357a94e79368ad464ce
edtheshed <edward.rixon@codurance.com>
2020-02-13 11:59:11 +0000

    added webapp and context config annotations

--- src/test/java/com/codurance/guru/audits/AuditorTest.java
+++ src/test/java/com/codurance/guru/audits/AuditorTest.java
@@ -12,13 +12,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
         classes = GuruApplication.class)
+@WebAppConfiguration
+@ContextConfiguration
 public class AuditorTest {
 
     @Autowired
